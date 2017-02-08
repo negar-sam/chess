f1d2d2c695b38b5f3acf62eeef4f0c3f67b713c8
Negar Samimian <negar_nodety@yahoo.com>
2017-02-08 03:33:40 +0330

    login and registration added

--- chess/chessapp/models.py
+++ chess/chessapp/models.py
@@ -1,12 +1,8 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
-class Users(models.Model):
-	mail = models.CharField( max_length=50 , primary_key=True)
-	name = models.CharField( max_length=50 )
-	password = models.CharField( max_length=30 )
 class Games(models.Model):
 	status = models.CharField(max_length=100) 
-	user = models.ForeignKey(Users)
+	user = models.ForeignKey(User)
 	
--- chess/chessapp/views.py
+++ chess/chessapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    return HttpResponse("test page")
